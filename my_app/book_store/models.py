0e1df933a32296ea11d5878830a2c0c115e28aea
hatikuay <ercan.erkalkan@marmara.edu.tr>
2023-05-13 21:22:53 +0300

    newCommit

--- my_app/book_store/models.py
+++ my_app/book_store/models.py
@@ -1,4 +1,5 @@
 from my_app import db
+from datetime import datetime
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,13 +22,15 @@ class Book(db.Model):
             "available_quantity": self.inventory.available_quantity if self.inventory else 0,
         }
         return book_dict
-    
+
     @staticmethod
     def create_book(title, author, publication_year):
-        new_book = Book(title=title, author=author, publication_year=publication_year)
-        db.session.add(new_book)        
-        initial_inventory = Inventory(book=new_book,quantity=0, available_quantity=0)
-        db.session.add(initial_inventory)        
+        new_book = Book(title=title, author=author,
+                        publication_year=publication_year)
+        db.session.add(new_book)
+        initial_inventory = Inventory(
+            book=new_book, quantity=0, available_quantity=0)
+        db.session.add(initial_inventory)
         db.session.commit()
         return new_book.to_dict()
 
@@ -59,6 +62,43 @@ class Inventory(db.Model):
         inventory = Inventory.query.filter_by(book_id=book_id).first()
         inventory.available_quantity += quantity
         db.session.commit()
-       
+
     def __repr__(self):
         return f'<Book {self.available_quantity}>'
+
+class BookOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeginKey('book.id'), nullable=False)
+    quantity_ordered = db.Column(db.Integer, nullable=False)
+    order_date = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+
+    def __init__(self, book_id, quantity_ordered):
+        self.book_id = book_id
+        self.quantity_ordered = quantity_ordered
+
+    def save(self):
+        inventory = Inventory.query.filter_by(book_id=self.book_id).first()
+        if not inventory:
+            inventory = Inventory(book_id=self.book_id, quantity=0)
+            db.session.add(inventory)
+
+        inventory.quantity += self.quantity_ordered
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        inventory = Inventory.query.filter_by(book_id=self.book_id).first()
+        if inventory:
+            inventory.quantity -= self.quantity_ordered
+            db.session.delete(self)
+            db.session.commit()
+    
+    def to_dict(self):
+        book_order_dict = {
+            "id": self.id,
+            "book_id": self.book_id,
+            "quantity_ordered": self.quantity_ordered,
+            "order_date": self.order_date
+        }
+        return book_order_dict
--- my_app/book_store/views.py
+++ my_app/book_store/views.py
@@ -2,7 +2,7 @@ from my_app import db
 from flask import Flask, render_template, request, redirect, url_for, request, jsonify, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from my_app.book_store.models import Book
+from my_app.book_store.models import Book, Inventory, BookOrder
 
 book_store = Blueprint('book_store', __name__)
 
@@ -103,4 +103,16 @@ def api_books_update():
             return jsonify(book.to_dict())
         return jsonify({"message": "not found"})
     return jsonify({"message": "no ID sent"})
+
+@book_store.route("/api/books/orders", methods = ["POST"])
+def api_books_orders():
+    data = request.json
+    book_id = data.get("book_id")
+    quantity_ordered = data.get("quantity_ordered")
+    
+    order = BookOrder(book_id=book_id,quantity_ordered=quantity_ordered)
+    order.save()
+    
+    response = {"message": "Order placed successfully."}
+    return jsonify(response), 201
 
